7bc2a589e7f4d4e992a3fd9cd1aa43cf4bf34db3
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-06-22 08:13:16 +0200

    Remove unnecessary provider configuration from provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,1 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    stackit = {
-      source  = "stackitcloud/stackit"
-      version = "0.55.0"
-    }
-  }
-}
 
-provider "stackit" {
-
-}
